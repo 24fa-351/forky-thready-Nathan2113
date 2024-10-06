666f7cf0c72581dcb761b21d6b72fcb4e26c1ec1
Nathan2113 <111931927+Nathan2113@users.noreply.github.com>
2024-10-06 16:54:07 -0700

    Updated pattern 1, 2 and results.txt

--- fork.c
+++ fork.c
@@ -3,10 +3,11 @@
 void pattern_1(int process_count) {
     srand(time(NULL));
 
-    pid_t *pids = malloc(process_count * sizeof(pid_t)); // Allocate memory for pid's stored
+    int sleep_timer = 0;
 
     for(int i = 0; i < process_count; i++) {
         pid_t pid = fork();
+        sleep_timer = (rand() % 8) + 1;
 
         if(pid < 0) { // pid will be < 0 if the fork failed
             printf("Fork failed");
@@ -16,35 +17,34 @@ void pattern_1(int process_count) {
         else if(pid == 0) { // do second process
             printf("Process 0 (%d) creating Process %u (%d)\n", getppid(), i + 1, getpid());
 
-            printf("Process %u beginning\n\n", i + 1);
+            printf("Process %u created: Sleeping for %u seconds...\n", i + 1, sleep_timer);
 
+            sleep(sleep_timer);
+
+
+            printf("Process %u (%d) exiting...\n\n", i + 1, getpid());
             exit(0); // End child process
         } // END else if(pid == 0)
         else {
-            pids[i] = pid;
+            wait(NULL);
         } // END - else
 
-        sleep(0.01); // Sleep for 0.01 seconds to prevent multiple fork outputs at the same time
     } // END - for(int i = 1; i <= process_count; i++)
 
-    for(int i = 0; i < process_count; i++) {
-        waitpid(pids[i], NULL, 0); // Waits for each child to finish
-        printf("Process %u (%d) exiting...\n", i + 1, pids[i]);
-
-        sleep((rand() % 8) + 1); // Sleep for a random number of seconds (from 1 - 8)
-    } // END - for(int i = 0; i < process_count; i++)
 }
 
 
 void pattern_2(int current_process, int process_count) {
     srand(time(NULL));
+    int sleep_timer = 0;
+    sleep_timer = (rand() % 8) + 1;
 
     if(current_process < process_count) {
-        printf("Process %u beginning\n", current_process);
+        printf("Process %u (%d) created: Sleeping for %u seconds...\n\n", current_process, getpid(), sleep_timer);
 
         pid_t pid = fork();
 
-        sleep((rand() % 8) + 1); // Sleep for a random number of seconds (from 1 - 8)
+        sleep(sleep_timer); // Sleep for a random number of seconds (from 1 - 8)
 
         if(pid < 0) { // pid will be < 0 if the fork failed
             printf("Fork failed");
@@ -52,7 +52,7 @@ void pattern_2(int current_process, int process_count) {
         } // END - if(pid < 0)
 
         else if(pid == 0) {
-            printf("Process %u (%d) creating Process %u (%d)\n\n", current_process, getppid(), current_process + 1, getpid());
+            printf("Process %u (%d) creating Process %u (%d)\n", current_process, getppid(), current_process + 1, getpid());
 
             pattern_2(current_process + 1, process_count);
 
